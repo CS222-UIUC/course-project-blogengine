03e34dd2d4785234a98316001e6cc372872ad7ec
goose614 <amy.wang10101@gmail.com>
2023-04-19 06:56:52 -0500

    added styling to each post on posts subpage; also implemented styling where if text was longer than certain length then I would only take first 100 characters as a snippet

--- blog/react-app/src/Posts.js
+++ blog/react-app/src/Posts.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
+import './Posts.css';
 
 // import axios from "axios";
 
@@ -27,15 +28,42 @@ export default function Posts() {
     // });
 
     return (
-        <div className='container'>
-            <h1>Posts</h1>
-                {blogs.map(blog => 
-                    <div>
-                        <h1>{blog.title}</h1>
-                        <p>{blog.content}</p>
-                        <Link to={`/posts/${blog.slug}`}>Read More</Link> {/* Use the Link component to link to the individual blog post page */}
-                    </div>
-                )}
+        <div>
+            <h1 id='header'>Posts</h1>
+            {blogs.map(blog =>
+                <div className='card'>
+                    {/* <div className="container-sm"> */}
+                        <h3>
+                            <Link to={`/posts/${blog.slug}`} className="nowrap-title">
+                                {blog.title}
+                            </Link>
+                        </h3>
+                        <div className="snippet">
+                            {/* <p dangerouslySetInnerHTML={{ __html: blog.body }}></p> */}
+                            <p>{blog.content.length > 100 ? blog.content.substring(0, 100) + "..." : blog.content}</p>
+                            <Link to={`/posts/${blog.slug}`} className="nowrap-body">
+                                {blog.content.length > 100 ? "Read more" : "Go to post"}{" "}
+                            {/* <i className="fas fa-arrow-circle-right"></i> */}
+                            </Link>
+                        </div>
+                    {/* </div> */}
+                </div>
+            )}
         </div>
+        // <div className='container'>
+        //     <h1>Posts</h1>
+        //         {blogs.map(blog => 
+        //             <div>
+        //                 <h1>{blog.title}</h1>
+        //                 <p>{blog.content}</p>
+        //                 <Link to={`/posts/${blog.slug}`}>Read More</Link> {/* Use the Link component to link to the individual blog post page */}
+        //             </div>
+        //         )}
+        // </div>
     )
 }
+
+// <Link to={`/posts/${blog.slug}`} className="nowrap-body">
+// {blog.content.length > 100 ? "Read more" : "Go to post"}{" "}
+// {/* <i className="fas fa-arrow-circle-right"></i> */}
+// </Link>
